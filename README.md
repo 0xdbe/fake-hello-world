566e2875e6be919057ff656454c33262456b8980
0xdbe <23268141+0xdbe@users.noreply.github.com>
2024-06-10 16:33:03 +0200

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Fake Hello World
+
+https://jsr.io/badges/@0xdbe/fake-hello-world
+
+A fake package to test JSR
+
---
+++ mod.ts
@@ -0,0 +1,7 @@
+// mod.ts
+import { bold } from "@std/fmt/colors";
+
+/** Returns `Hello World` in bold */
+export function getHelloWorld(): string {
+  return bold("Hello World");
+}
